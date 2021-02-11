523735460c55163736a17cd2f958202f4ffaf198
Hipoppoltark <48064845+Hipoppoltark@users.noreply.github.com>
2021-02-11 19:22:53 +0300

    Update main.py
    Масштаб

--- main.py
+++ main.py
@@ -12,17 +12,27 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
         self.getImage('0.002,0.002')
-        self.initUI()
         self.spn = 0.002
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
-            self.spn *= 2
-            print(self.spn)
+            try:
+                self.spn *= 2
+                self.getImage(str(self.spn) + ',' + str(self.spn))
+            except BaseException:
+                pass
         elif event.key() == Qt.Key_PageDown:
-            self.spn /= 2
-        self.getImage(str(self.spn) + ',' + str(self.spn))
+            try:
+                self.spn /= 2
+                self.getImage(str(self.spn) + ',' + str(self.spn))
+            except BaseException:
+                pass
 
     def getImage(self, spn):
         params = {
@@ -43,23 +53,14 @@ class Example(QWidget):
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
+        try:
+            self.image.destroy()
+        except BaseException:
+            pass
         self.pixmap = QPixmap(self.map_file)
         self.image.setPixmap(self.pixmap)
-        
+        print(self.pixmap)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
@@ -70,4 +71,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec()))
